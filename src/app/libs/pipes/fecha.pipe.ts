1970f2f2571657122168a727844c5b21b0bd0de3
Narbok <narbok29@gmail.com>
2022-09-22 20:43:19 -0500

    ok

--- src/app/libs/pipes/fecha.pipe.ts
+++ src/app/libs/pipes/fecha.pipe.ts
@@ -7,17 +7,19 @@ import * as moment from 'moment';
 export class FechaPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    let now = moment().format('dddd')
+    
+    moment.locale('es-mx'); //cambia a español mexico
+    //let now = moment().format('dddd')
     // let now = moment().format('MMMM Do YYYY, h:mm:ss a')
     // let now = moment().format('dddd')
     // let now = moment().format("MMM Do YY")
-    // let now = moment().startOf('day').fromNow()
+    //let now = moment().startOf('day').fromNow()
     // let now = moment().subtract(10, 'days').calendar()
     // let now = moment().subtract(1, 'days').calendar()
-    // let now = moment().calendar()
-    // let now = moment().add(10, 'days').calendar()
+    let now = moment().calendar()
+    //let now = moment().add(10, 'days').calendar()
     // let now = moment().format('LT')
-    // let now = moment().format('LTS')
+    // let now = moment().format('LT')
     return now;
   }
 }
